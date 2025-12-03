b1ac61ef206a3997223e0f226098fa081f0869de
Ben Ratelade <ben@fresho.com>
2025-12-04 05:40:39 +1100

    Solution for day 3 - not working for part 2 yet

--- lib/day_3/solver.rb
+++ lib/day_3/solver.rb
@@ -1,2 +1,69 @@
 # frozen_string_literal: true
-class Solver;end
+
+class Solver
+  attr_reader :battery_banks
+
+  def initialize(file)
+    @file = file
+    @battery_banks = []
+    assign_battery_banks
+  end
+
+  def solve_part_1
+    battery_banks.sum do |battery_bank|
+      extract_joltage_from_battery_bank(battery_bank)
+    end
+  end
+
+  def solve_part_2
+    all_joltages = []
+    threads = @battery_banks.map do |battery_bank|
+      threads << Thread.new do
+        all_joltages << extract_joltage_from_battery_bank_for_part_2(battery_bank)
+      end
+    end
+
+    threads.each(&:join)
+    all_joltages.sum
+  end
+
+  def assign_battery_banks
+    File.readlines(@file).each do |line|
+      @battery_banks << line.strip.chars.map(&:to_i)
+    end
+  end
+
+  def extract_joltage_from_battery_bank(battery_bank)
+    all_maxes = []
+
+    battery_bank.length.times do |index|
+      all_maxes << "#{battery_bank[0...index].max}#{battery_bank[index..].max}".to_i
+    end
+
+    all_maxes.max
+  end
+
+  def extract_joltage_from_battery_bank_for_part_2(battery_bank)
+    first_available_index = 0
+    last_used_index = battery_bank.length - 12
+    max_array = battery_bank[last_used_index..]
+
+    (0...12).each do |position|
+      new_positions_to_consider = battery_bank[first_available_index...last_used_index]
+      new_max_for_position = new_positions_to_consider.max
+      index_of_new_max = new_positions_to_consider.index(new_max_for_position) + first_available_index
+
+      if new_max_for_position > max_array[position]
+        max_array[position] = new_max_for_position
+        first_available_index = index_of_new_max + 1
+        last_used_index += 1
+      elsif new_max_for_position == max_array[position]
+        last_used_index += 1
+      elsif new_max_for_position < max_array[position]
+        break
+      end
+    end
+
+    max_array.join.to_i
+  end
+end
--- spec/lib/day_3/solver_spec.rb
+++ spec/lib/day_3/solver_spec.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 require "day_3/solver"
 require "pry"
 
@@ -6,11 +7,84 @@ RSpec.describe Solver do
   before do
     File.open("test-file.txt", "w") do |file|
       file << <<~FILE_CONTENT
+        987654321111111
+        811111111111119
+        234234234234278
+        818181911112111
       FILE_CONTENT
     end
   end
 
   after do
     File.delete("test-file.txt")
+  end
+
+  describe ".solve_part_1" do
+    it "returns the expected result for part 1" do
+      solver = Solver.new("test-file.txt")
+
+      expect(solver.solve_part_1).to eq(357)
+    end
+  end
+
+  describe ".solve_part_2" do
+    it "returns the expected result for part 2" do
+      solver = Solver.new("test-file.txt")
+
+      expect(solver.solve_part_2).to eq(3_121_910_778_619)
+    end
+  end
+
+  describe "initialize" do
+    it "assigns a list of battery_banks" do
+      solver = Solver.new("test-file.txt")
+
+      expect(solver.battery_banks).to eq(
+        [
+          [9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1, 1],
+          [8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9],
+          [2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 7, 8],
+          [8, 1, 8, 1, 8, 1, 9, 1, 1, 1, 1, 2, 1, 1, 1],
+        ],
+      )
+    end
+  end
+
+  describe "extract_joltage_from_battery_bank" do
+    it "extracts the joltage from a battery bank" do
+      solver = Solver.new("test-file.txt")
+
+      expect(solver.extract_joltage_from_battery_bank([9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1, 1])).to eq(98)
+      expect(solver.extract_joltage_from_battery_bank([8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9])).to eq(89)
+      expect(solver.extract_joltage_from_battery_bank([2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 7, 8])).to eq(78)
+      expect(solver.extract_joltage_from_battery_bank([8, 1, 8, 1, 8, 1, 9, 1, 1, 1, 1, 2, 1, 1, 1])).to eq(92)
+    end
+  end
+
+  describe "extract_joltage_from_battery_bank_for_part_2" do
+    it "extracts the joltage from a battery bank" do
+      solver = Solver.new("test-file.txt")
+
+      expect(
+        solver.extract_joltage_from_battery_bank_for_part_2(
+          [9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1, 1],
+        ),
+      ).to eq(987_654_321_111)
+      expect(
+        solver.extract_joltage_from_battery_bank_for_part_2(
+          [8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9],
+        ),
+      ).to eq(811_111_111_119)
+      expect(
+        solver.extract_joltage_from_battery_bank_for_part_2(
+          [2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 7, 8],
+        ),
+      ).to eq(434_234_234_278)
+      expect(
+        solver.extract_joltage_from_battery_bank_for_part_2(
+          [8, 1, 8, 1, 8, 1, 9, 1, 1, 1, 1, 2, 1, 1, 1],
+        ),
+      ).to eq(888_911_112_111)
+    end
   end
 end
