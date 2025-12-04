b07f6899ffbafc052d91dbfba38537ea40c5e95a
Ben Ratelade <ben@fresho.com>
2025-12-04 20:04:16 +1100

    day 4 part 2

--- lib/day_4/solver.rb
+++ lib/day_4/solver.rb
@@ -25,7 +25,32 @@ class Solver
     valid_cells_count
   end
 
-  def solve_part_2;end
+  def solve_part_2
+    total_removed_cells_count = 0
+    removable_roll_coordinates = []
+    loop do
+      total_removed_cells_count += removable_roll_coordinates.count
+      removable_roll_coordinates.each do |coordinates_array|
+        x_index, y_index = coordinates_array
+        @map[x_index, y_index] = "."
+      end
+      removable_roll_coordinates = []
+
+      @map.each do |value, x_index, y_index|
+        next unless value == "@"
+
+        surrounded_by_rolls_count = 0
+        @map.surrounding_values_for(x_index, y_index).each do |surrounding_cell|
+          surrounded_by_rolls_count += 1 if surrounding_cell[:value] == "@"
+        end
+        removable_roll_coordinates << [x_index, y_index] if surrounded_by_rolls_count < 4
+      end
+
+      break unless removable_roll_coordinates.count.positive?
+    end
+
+    total_removed_cells_count
+  end
 
   private
 
