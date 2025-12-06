523b6276ad4661ddf73681a40979de4a2ecc416d
Ben Ratelade <ben@fresho.com>
2025-12-07 06:19:21 +1100

    refactor for processing inputs and day 6 part 1

--- lib/day_6/solver.rb
+++ lib/day_6/solver.rb
@@ -13,19 +13,35 @@ class Solver
     @operations.sum(&:result)
   end
 
-  def solve_part_2; end
+  def solve_part_2
+    @operations.sum(&:cephalopod_result)
+  end
 
   class Operation
-    attr_reader :operand, :inputs
+    attr_reader :operand, :raw_inputs, :inputs, :cephalopod_inputs
 
-    def initialize(operand:, inputs:)
+    def initialize(operand:, raw_inputs:)
       @operand = operand
-      @inputs = inputs
+      @raw_inputs = raw_inputs
+      @inputs = process_inputs
+      @cephalopod_inputs = process_cephalopod_inputs
     end
 
     def result
       eval(@inputs.join(@operand)) # rubocop:disable Security/Eval
     end
+
+    def cephalopod_result
+      eval(@cephalopod_inputs.join(@operand)) # rubocop:disable Security/Eval
+    end
+
+    private
+
+    def process_inputs
+      @inputs = @raw_inputs.map {|characters| characters.join.to_i}
+    end
+
+    def process_cephalopod_inputs; end
   end
 
   private
@@ -33,16 +49,38 @@ class Solver
   def process_file
     operations = {}
 
-    File.readlines(@file).each do |line|
-      line.strip.split.each_with_index do |operation_input, index|
-        operations[index] ||= { operand: nil, inputs: [] }
+    lines = File.read(@file).split("\n")
+    operation_matches = lines.last.to_enum(:scan, /[\*\+]/).map { Regexp.last_match }
+    operation_matches.each_with_index do |match_data, index|
+      operations[index] = { raw_inputs: [], operand: nil }
+      operations[index][:operand] = match_data.match(0)
+    end
+    operation_indexes = operation_matches.map { |match| match.begin(0) - 1 }
 
-        if operation_input.match?(/\d+/)
-          operations[index][:inputs] << operation_input.to_i
-        else
-          operations[index][:operand] = operation_input
+    lines.each do |line|
+      next if line.match?(/\*/)
+
+      puts "processing line: #{line}"
+
+      line_input = []
+      input = []
+      line.each_char.with_index do |char, index|
+        if operation_indexes.include?(index)
+          line_input << input
+          input = []
+          next
         end
+
+        if index == (line.length - 1)
+          input << char
+          line_input << input
+          input = []
+        end
+
+        input << char
+
       end
+      line_input.each_with_index { |raw_input, index| operations[index][:raw_inputs] << raw_input }
     end
 
     @operations = operations.values.map do |operation_hash|
--- spec/lib/day_6/solver_spec.rb
+++ spec/lib/day_6/solver_spec.rb
@@ -6,12 +6,10 @@ require "pry"
 RSpec.describe Solver do
   before do
     File.open("test-file.txt", "w") do |file|
-      file << <<~FILE_CONTENT
-        123 328  51 64
-        45 64  387 23
-        6 98  215 314
-        *   +   *   +
-      FILE_CONTENT
+      file.puts "123 328  51 64 "
+      file.puts " 45 64  387 23 "
+      file.puts "  6 98  215 314"
+      file.puts "*   +   *   +  "
     end
   end
 
@@ -31,7 +29,7 @@ RSpec.describe Solver do
     it "returns the expected result for part 2" do
       solver = Solver.new("test-file.txt")
 
-      expect(solver.solve_part_2).to eq(nil)
+      expect(solver.solve_part_2).to eq(3_263_827)
     end
   end
 
@@ -46,6 +44,20 @@ RSpec.describe Solver do
       expect(solver.operations[1].operand).to eq("+")
       expect(solver.operations[1].inputs).to eq([328, 64, 98])
       expect(solver.operations[1].result).to eq(490)
+    end
+
+    it "assigns operations for part 2" do
+      solver = Solver.new("test-file.txt")
+
+      expect(solver.operations[0].operand).to eq("*")
+      expect(solver.operations[0].cephalopod_inputs).to eq([356, 24, 1])
+      expect(solver.operations[0].cephalopod_result).to eq(1058)
+
+      expect(solver.operations[1].operand).to eq("+")
+      expect(solver.operations[1].cephalopod_inputs).to eq([8, 248, 369])
+      expect(solver.operations[1].cephalopod_result).to eq(3_253_600)
+
+      expect(solver.operations[3].raw_inputs).to eq([["6", "4", " "], ["2", "3", " "], ["3", "1", "4"]])
     end
   end
 end
