ab475f5646c72817f7290126796023d3ebecd286
Ben Ratelade <ben@fresho.com>
2025-12-06 17:38:06 +1100

    Day 5, part 2!

--- lib/day_5/solver.rb
+++ lib/day_5/solver.rb
@@ -19,7 +19,45 @@ class Solver
     fresh_ingredients.count
   end
 
-  def solve_part_2; end
+  def solve_part_2
+    final_ranges = []
+    removed_ranges = []
+
+    @ranges.each do |range|
+      next if removed_ranges.include?(range)
+
+      # combine with all other ranges and repeat until the range does not get modified anymode
+      accumulator_range = range
+      accumulator_modified = true
+      while accumulator_modified
+        initial_accumulator_value = accumulator_range
+        @ranges.each do |range_to_merge|
+          next if removed_ranges.include?(range_to_merge)
+
+          combined_ranges = Solver.combine_ranges(accumulator_range, range_to_merge)
+
+          next unless combined_ranges
+
+          accumulator_range = combined_ranges
+          removed_ranges << range_to_merge
+        end
+        accumulator_modified = initial_accumulator_value != accumulator_range
+      end
+      final_ranges << accumulator_range
+    end
+
+    final_ranges.sum(&:count)
+  end
+
+  def self.combine_ranges(range_1, range_2)
+    if range_1.overlap?(range_2)
+      return (
+        [range_1.first, range_2.first].min..([range_1.end, range_2.end].max)
+      )
+    end
+
+    false
+  end
 
   private
 
--- spec/lib/day_5/solver_spec.rb
+++ spec/lib/day_5/solver_spec.rb
@@ -61,7 +61,37 @@ RSpec.describe Solver do
     it "returns the expected result for part 2" do
       solver = Solver.new("test-file.txt")
 
-      expect(solver.solve_part_2).to eq(nil)
+      expect(solver.solve_part_2).to eq(14)
+    end
+  end
+
+  describe ".combine_ranges" do
+    it "combines_ranges if range_2 is included in range_1 intersect" do
+      range_1 = (3..5)
+      range_2 = (4..5)
+
+      expect(Solver.combine_ranges(range_1, range_2)).to eq(3..5)
+    end
+
+    it "combines_ranges if range_1 and range_2 intersect from the start" do
+      range_1 = (3..5)
+      range_2 = (1..3)
+
+      expect(Solver.combine_ranges(range_1, range_2)).to eq(1..5)
+    end
+
+    it "combines_ranges if range_1 and range_2 intersect from the end" do
+      range_1 = (3..5)
+      range_2 = (5..7)
+
+      expect(Solver.combine_ranges(range_1, range_2)).to eq(3..7)
+    end
+
+    it "does not combine ranges if they do not intersect" do
+      range_1 = (3..5)
+      range_2 = (6..7)
+
+      expect(Solver.combine_ranges(range_1, range_2)).to eq(false)
     end
   end
 end
