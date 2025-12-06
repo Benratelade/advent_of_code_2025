0b098d6e278f8e3f940ec3c4c2803f03ffc50ec8
Ben Ratelade <ben@fresho.com>
2025-12-06 18:42:35 +1100

    day 6 part 1

--- lib/day_6/solver.rb
+++ lib/day_6/solver.rb
@@ -1,3 +1,52 @@
 # frozen_string_literal: true
 
-class Solver; end
+class Solver
+  attr_reader :operations
+
+  def initialize(file)
+    @file = file
+
+    process_file
+  end
+
+  def solve_part_1
+    @operations.sum(&:result)
+  end
+
+  def solve_part_2; end
+
+  class Operation
+    attr_reader :operand, :inputs
+
+    def initialize(operand:, inputs:)
+      @operand = operand
+      @inputs = inputs
+    end
+
+    def result
+      eval(@inputs.join(@operand)) # rubocop:disable Security/Eval
+    end
+  end
+
+  private
+
+  def process_file
+    operations = {}
+
+    File.readlines(@file).each do |line|
+      line.strip.split.each_with_index do |operation_input, index|
+        operations[index] ||= { operand: nil, inputs: [] }
+
+        if operation_input.match?(/\d+/)
+          operations[index][:inputs] << operation_input.to_i
+        else
+          operations[index][:operand] = operation_input
+        end
+      end
+    end
+
+    @operations = operations.values.map do |operation_hash|
+      Operation.new(**operation_hash)
+    end
+  end
+end
--- spec/lib/day_6/solver_spec.rb
+++ spec/lib/day_6/solver_spec.rb
@@ -6,11 +6,46 @@ require "pry"
 RSpec.describe Solver do
   before do
     File.open("test-file.txt", "w") do |file|
-      file << ""
+      file << <<~FILE_CONTENT
+        123 328  51 64
+        45 64  387 23
+        6 98  215 314
+        *   +   *   +
+      FILE_CONTENT
     end
   end
 
   after do
     File.delete("test-file.txt")
+  end
+
+  describe ".solve_part_1" do
+    it "returns the expected result for part 1" do
+      solver = Solver.new("test-file.txt")
+
+      expect(solver.solve_part_1).to eq(4_277_556)
+    end
+  end
+
+  describe ".solve_part_2" do
+    it "returns the expected result for part 2" do
+      solver = Solver.new("test-file.txt")
+
+      expect(solver.solve_part_2).to eq(nil)
+    end
+  end
+
+  describe "initialize" do
+    it "assigns operations" do
+      solver = Solver.new("test-file.txt")
+
+      expect(solver.operations[0].operand).to eq("*")
+      expect(solver.operations[0].inputs).to eq([123, 45, 6])
+      expect(solver.operations[0].result).to eq(33_210)
+
+      expect(solver.operations[1].operand).to eq("+")
+      expect(solver.operations[1].inputs).to eq([328, 64, 98])
+      expect(solver.operations[1].result).to eq(490)
+    end
   end
 end
