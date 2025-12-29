32c38fa182bf8a2e36541a6a474c8489555b8d70
Ben Ratelade <ben@fresho.com>
2025-12-29 19:45:27 +1100

    Process input for day 11

--- lib/day_11/solver.rb
+++ lib/day_11/solver.rb
@@ -1,3 +1,32 @@
 # frozen_string_literal: true
 
-class Solver; end
+class Solver
+  attr_reader :devices
+
+  def initialize(file)
+    @file = file
+
+    process_devices
+  end
+
+  def solve_part_1
+    minimum_count = @machines.map do |machine|
+      press_all_buttons_for_machine(machine)
+    end
+
+    minimum_count.sum
+  end
+
+  def solve_part_2; end
+
+  private
+
+  def process_devices
+    @devices = {}
+    File.readlines(@file).each do |line|
+      input = line.match(/(\w{3}):/)[1]
+      outputs = line[4..].split
+      @devices[input] = outputs
+    end
+  end
+end
--- spec/lib/day_11/solver_spec.rb
+++ spec/lib/day_11/solver_spec.rb
@@ -6,11 +6,59 @@ require "pry"
 RSpec.describe Solver do
   before do
     File.open("test-file.txt", "w") do |file|
-      file << ""
+      file << <<~FILE_CONTENT
+        aaa: you hhh
+        you: bbb ccc
+        bbb: ddd eee
+        ccc: ddd eee fff
+        ddd: ggg
+        eee: out
+        fff: out
+        ggg: out
+        hhh: ccc fff iii
+        iii: out
+      FILE_CONTENT
     end
   end
 
   after do
     File.delete("test-file.txt")
+  end
+
+  describe ".solve_part_1" do
+    it "returns the expected result for part 1" do
+      solver = Solver.new("test-file.txt")
+
+      expect(solver.solve_part_1).to eq(5)
+    end
+  end
+
+  describe ".solve_part_2" do
+    it "returns the expected result for part 2" do
+      solver = Solver.new("test-file.txt")
+
+      expect(solver.solve_part_2).to eq(nil)
+    end
+  end
+
+  describe "initialize" do
+    it "instantiates devices" do
+      solver = Solver.new("test-file.txt")
+
+      expect(solver.devices).to eq(
+        {
+          "aaa" => %w[you hhh],
+          "you" => %w[bbb ccc],
+          "bbb" => %w[ddd eee],
+          "ccc" => %w[ddd eee fff],
+          "ddd" => %w[ggg],
+          "eee" => %w[out],
+          "fff" => %w[out],
+          "ggg" => %w[out],
+          "hhh" => %w[ccc fff iii],
+          "iii" => %w[out],
+        },
+      )
+    end
   end
 end
