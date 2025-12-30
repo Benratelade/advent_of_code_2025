f1684b6ba5a3493dc9496676eea70be77efa2eff
Ben Ratelade <ben@fresho.com>
2025-12-30 14:58:39 +1100

    Hmmm part 2 not working

--- lib/day_11/solver.rb
+++ lib/day_11/solver.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Solver
-  attr_reader :devices
+  attr_reader :devices, :reverse_devices
 
   def initialize(file)
     @file = file
@@ -10,18 +10,52 @@ class Solver
   end
 
   def solve_part_1
+    paths = follow_all_paths_between("you", "out")
+    paths.uniq.count
+  end
+
+  def solve_part_2
+    find_all_path_starts_between("fft", "svr")
+  end
+
+  def find_all_path_starts_between(end_address, start_address = "svr")
     paths = []
 
-    start = "you"
-    incomplete_paths = { start => "you" }
+    incomplete_reverse_paths = { end_address => end_address }
+
+    loop do
+      break if incomplete_reverse_paths.empty?
+
+      new_incomplete_paths = {}
+      incomplete_reverse_paths.each do |incomplete_path, previous_inputs|
+        @reverse_devices[previous_inputs].each do |previous_3_chars|
+          new_path = [previous_3_chars, incomplete_path].join(" ")
+          if previous_3_chars == start_address || previous_3_chars == "svr"
+            paths << new_path
+          else
+            new_incomplete_paths[new_path] = previous_3_chars
+          end
+        end
+      end
+      incomplete_reverse_paths = new_incomplete_paths
+    end
+
+    paths
+  end
+
+  def follow_all_paths_between(start_address, end_address)
+    paths = []
+
+    start = start_address
+    incomplete_paths = { start => start }
     loop do
       break if incomplete_paths.empty?
 
       new_incomplete_paths = {}
       incomplete_paths.each do |incomplete_path, next_output|
-        devices[next_output].each do |next_3_chars|
+        @devices[next_output].each do |next_3_chars|
           new_path = incomplete_path + next_3_chars
-          if next_3_chars == "out"
+          if next_3_chars == end_address || next_3_chars == "out"
             paths << new_path
           else
             new_incomplete_paths[new_path] = next_3_chars
@@ -31,19 +65,52 @@ class Solver
       incomplete_paths = new_incomplete_paths
     end
 
-    paths.uniq.count
+    puts paths.uniq
+    paths.uniq
   end
 
-  def solve_part_2; end
+  def follow_all_paths_between_and_stop_when_found(start_address, end_address)
+    paths = []
+
+    start = start_address
+    end_address_reached = false
+    incomplete_paths = { start => start }
+    loop do
+      break if end_address_reached
+
+      new_incomplete_paths = {}
+      incomplete_paths.each do |incomplete_path, next_output|
+        @devices[next_output].each do |next_3_chars|
+          new_path = incomplete_path + next_3_chars
+          if next_3_chars == end_address
+            paths << new_path
+            end_address_reached = true
+            break
+          else
+            new_incomplete_paths[new_path] = next_3_chars
+          end
+        end
+      end
+      incomplete_paths = new_incomplete_paths
+    end
+
+    puts paths.uniq
+    paths.uniq
+  end
 
   private
 
   def process_devices
     @devices = {}
+    @reverse_devices = {}
     File.readlines(@file).each do |line|
       input = line.match(/(\w{3}):/)[1]
       outputs = line[4..].split
       @devices[input] = outputs
+      outputs.each do |output|
+        @reverse_devices[output] ||= []
+        @reverse_devices[output] << input
+      end
     end
   end
 end
--- spec/lib/day_11/solver_spec.rb
+++ spec/lib/day_11/solver_spec.rb
@@ -19,10 +19,29 @@ RSpec.describe Solver do
         iii: out
       FILE_CONTENT
     end
+
+    File.open("test-file-2.txt", "w") do |file|
+      file << <<~FILE_CONTENT
+        svr: aaa bbb
+        aaa: fft
+        fft: ccc
+        bbb: tty
+        tty: ccc
+        ccc: ddd eee
+        ddd: hub
+        hub: fff
+        eee: dac
+        dac: fff
+        fff: ggg hhh
+        ggg: out
+        hhh: out
+      FILE_CONTENT
+    end
   end
 
   after do
     File.delete("test-file.txt")
+    File.delete("test-file-2.txt")
   end
 
   describe ".solve_part_1" do
@@ -35,9 +54,10 @@ RSpec.describe Solver do
 
   describe ".solve_part_2" do
     it "returns the expected result for part 2" do
-      solver = Solver.new("test-file.txt")
+      pending "That doesn't work yet"
+      solver = Solver.new("test-file-2.txt")
 
-      expect(solver.solve_part_2).to eq(nil)
+      expect(solver.solve_part_2).to eq(2)
     end
   end
 
@@ -57,6 +77,28 @@ RSpec.describe Solver do
           "ggg" => %w[out],
           "hhh" => %w[ccc fff iii],
           "iii" => %w[out],
+        },
+      )
+    end
+
+    it "instantiates a reverse devices map" do
+      solver = Solver.new("test-file-2.txt")
+
+      expect(solver.reverse_devices).to eq(
+        {
+          "aaa" => %w[svr],
+          "bbb" => %w[svr],
+          "fft" => %w[aaa],
+          "ccc" => %w[fft tty],
+          "tty" => %w[bbb],
+          "ddd" => %w[ccc],
+          "eee" => %w[ccc],
+          "hub" => %w[ddd],
+          "fff" => %w[hub dac],
+          "dac" => %w[eee],
+          "ggg" => %w[fff],
+          "hhh" => %w[fff],
+          "out" => %w[ggg hhh],
         },
       )
     end
