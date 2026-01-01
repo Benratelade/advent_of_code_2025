c549b7ccce1ba721f26f169fbe75bfd9e4bd92e1
Ben Ratelade <ben@fresho.com>
2026-01-01 12:41:48 +1100

    Day 12 part 1 done!

--- lib/day_12/solver.rb
+++ lib/day_12/solver.rb
@@ -1,3 +1,76 @@
 # frozen_string_literal: true
 
-class Solver; end
+class Solver
+  attr_reader :shapes, :regions
+
+  def initialize(file)
+    @file = file
+    @shapes = []
+    @regions = []
+
+    process_presents_and_regions
+  end
+
+  def solve_part_1
+    @regions.filter do |region|
+      region.total_necessary_space <= region.area
+    end.count
+  end
+
+  def solve_part_2; end
+
+  private
+
+  def process_presents_and_regions
+    current_shape_description = []
+    File.readlines(@file).each do |line|
+      if line.strip.empty?
+        @shapes << Shape.new(shape_description: current_shape_description)
+        current_shape_description = []
+        next
+      end
+
+      if line.match?(/\d+x\d+/)
+        process_region(line)
+        next
+      elsif line.match?(/\d:/)
+        next
+      end
+
+      current_shape_description << line.strip
+    end
+  end
+
+  def process_region(line)
+    @regions << Region.new(line, shapes: @shapes)
+  end
+
+  class Shape
+    attr_reader :area
+
+    def initialize(shape_description:)
+      @shape_description = shape_description
+      @area = @shape_description.join("\n").count("#")
+    end
+  end
+
+  class Region
+    attr_reader :area, :presents_list
+
+    def initialize(region_description, shapes:)
+      size, presents_counts = region_description.split(": ")
+
+      @area = size.split("x").map(&:to_i).reduce(:*)
+      @presents_list = {}
+      presents_counts.split.each_with_index do |count, index|
+        @presents_list[shapes[index]] = count.to_i
+      end
+    end
+
+    def total_necessary_space
+      @presents_list.sum do |shape, count|
+        shape.area * count
+      end
+    end
+  end
+end
--- spec/lib/day_12/solver_spec.rb
+++ spec/lib/day_12/solver_spec.rb
@@ -6,11 +6,79 @@ require "pry"
 RSpec.describe Solver do
   before do
     File.open("test-file.txt", "w") do |file|
-      file << ""
+      file << <<~FILE_CONTENT
+        0:
+        ###
+        ##.
+        ##.
+
+        1:
+        ###
+        ##.
+        .##
+
+        2:
+        .##
+        ###
+        ##.
+
+        3:
+        ##.
+        ###
+        ##.
+
+        4:
+        ###
+        #..
+        ###
+
+        5:
+        ###
+        .#.
+        ###
+
+        4x4: 0 0 0 0 2 0
+        12x5: 1 0 1 0 2 2
+        12x5: 1 0 1 0 3 2
+      FILE_CONTENT
     end
   end
 
   after do
     File.delete("test-file.txt")
+  end
+
+  describe ".solve_part_1" do
+    it "returns the expected result for part 1" do
+      pending "the naive approach of considering the size of all presents without worrying about shape works. The example doesn't"
+      solver = Solver.new("test-file.txt")
+
+      expect(solver.solve_part_1).to eq(2)
+    end
+  end
+
+  describe ".solve_part_2" do
+    it "returns the expected result for part 2" do
+      solver = Solver.new("test-file.txt")
+
+      expect(solver.solve_part_2).to eq(nil)
+    end
+  end
+
+  describe "initialize" do
+    it "instantiates shapes and regions" do
+      solver = Solver.new("test-file.txt")
+
+      expect(solver.shapes.count).to eq(6)
+      expect(solver.shapes[0].area).to eq(7)
+      expect(solver.shapes[1].area).to eq(7)
+      expect(solver.shapes[2].area).to eq(7)
+
+      expect(solver.regions.count).to eq(3)
+      expect(solver.regions[0].area).to eq(16)
+      expect(solver.regions[0].area).to eq(16)
+      expect(solver.regions[1].area).to eq(60)
+      expect(solver.regions[2].area).to eq(60)
+    end
   end
 end
